dfe1ebdc3d0fc08d0575d324666847c4252411c5
Amachirin102 <Amachirin102@gmail.com>
2022-12-23 14:34:21 -0500

    First Commit

---
+++ app/src/main/java/com/amachirin102/weight_loss_calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amachirin102.weight_loss_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
